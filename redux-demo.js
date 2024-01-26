4c69eefcee1fce237a7351bbfa627f7bf997debc
Sonu Kumar <sksonu8544@gmail.com>
2024-01-26 22:52:37 +0530

    core redux work this way

--- redux-demo.js
+++ redux-demo.js
@@ -1,7 +1,7 @@
 const redux = require('redux');
 
 // Reducer function
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 10 }, action) => {
 
     if (action.type === 'INCREMENT') {
         return {
@@ -10,25 +10,27 @@ const counterReducer = (state = { counter: 0 }, action) => {
     }
     else {
         return {
-            counter: state.counter - 10
+            counter: state.counter - 1
         }
     }
+    return state
 };
 
 // Create Redux store
 const store = redux.createStore(counterReducer);
 
 // Initial state
-console.log(store.getState());
+console.log("initial State", store.getState());
 
 // Subscriber function
 const counterSubscriber = () => {
     const latestState = store.getState();
-    console.log(latestState);
+    console.log(latestState.type, latestState);
 };
 
 // Subscribe to the store
 store.subscribe(counterSubscriber);
 
 // Dispatch an action (you need to provide an action with a type)
+store.dispatch({ type: 'INCREMENT' });
 store.dispatch({ type: 'DECREMENT' });
